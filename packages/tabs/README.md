950ef36c2a0ff87914d7d5f46ecbd80670c5d3e6
James Lovallo <lovallo.james@gmail.com>
2022-08-12 15:38:32 -0400

    add snappy-tabs

---
+++ packages/tabs/README.md
@@ -0,0 +1,95 @@
+# &lt;snappy-tabs&gt;
+
+A typing element that's easy to use and customize. 0.85kb (brotli).
+
+[Demo](https://codepen.io/jameslovallo/pen/jOzvQLg)
+
+## Why Make Another Tab/Accordion Element?
+
+The short answer: accessibility. This component uses only unmodified `<details>` and `<summary>` elements, which are fully semantic and accessible. It's also _tiny_.
+
+## Features
+
+1. Tab and Accordion views using `type="tabs"` or `type="accordion"`.
+2. Breakpoint-based view switching using `breakpoint="768px"` (provide unit and value).
+3. Floating tab indicator.
+4. Directional tab animations.
+5. Part selectors for easy customization.
+
+## Installation
+
+Option 1: As a package.
+
+```sh
+npm i @snappywc/tabs
+```
+
+```js
+import '@snappywc/tabs'
+```
+
+Option 2: In your markup.
+
+```html
+<script type="module">
+	import '//unpkg.com/@snappywc/tabs'
+</script>
+```
+
+## Usage
+
+It's really, _really_ easy.
+
+```html
+<snappy-tabs type="tabs">
+	<details open>
+		<!--open by default-->
+		<summary><h3>Tab 1</h3></summary>
+		<!--will be part="tab"-->
+		<div>
+			<!--will be part="panel"-->
+			<h4>Panel 1</h4>
+			<p>Panel 1 content.</p>
+		</div>
+	</details>
+	<details>
+		<summary><h3>Tab 2</h3></summary>
+		<div>
+			<h4>Panel 2</h4>
+			<p>Panel 2 content.</p>
+		</div>
+	</details>
+	<details>
+		<summary><h3>Tab 2</h3></summary>
+		<div>
+			<h4>Panel 2</h4>
+			<p>Panel 2 content.</p>
+		</div>
+	</details>
+</snappy-tabs>
+```
+
+## Customization
+
+The default styles and part selectors for this element were carefully considered to be as minimal and un-opinionated as possible.
+
+### Parts
+
+The following parts are available for styling.
+
+| Part      | Description                 |
+| --------- | --------------------------- |
+| tab       | The tab or accordion label. |
+| panel     | The tab or accordion panel. |
+| indicator | The floating tab indicator. |
+
+Example Use
+
+```css
+snappy-tabs::part(tab) {
+	text-align: center;
+}
+snappy-tabs::part(indicator) {
+	background: darkslateblue;
+}
+```
---
+++ packages/tabs/snappy-tabs.js
@@ -0,0 +1,129 @@
+export default (() => {
+	!customElements.get('snappy-tabs') &&
+		customElements.define(
+			'snappy-tabs',
+			class extends HTMLElement {
+				constructor() {
+					super()
+					this.sr = this.attachShadow({ mode: 'open' })
+					this.type = this.getAttribute('type')
+
+					const bp = this.getAttribute('breakpoint')
+					if (bp) {
+						const mq = matchMedia(`(min-width: ${bp})`)
+						this.type = mq.matches ? 'tabs' : 'accordion'
+						mq.addEventListener('change', (e) => {
+							this.type = mq.matches ? 'tabs' : 'accordion'
+							this.connectedCallback()
+						})
+					}
+
+					this.template = `
+						<style>
+							:host {
+								border: 1px solid currentcolor;
+								border-radius: 4px;
+								display: block;
+								overflow: hidden;
+								position: relative;
+							}
+							details[open] summary {
+								border-bottom: 1px solid currentcolor;
+								font-weight: bold;
+							}
+							details:not(:last-of-type) {
+								border-bottom: 1px solid currentcolor
+							}
+							summary {
+								cursor: pointer;
+								box-sizing: border-box;
+								display: block;
+								padding: 1rem;
+							}
+							summary:marker { display: none }
+							summary::-webkit-details-marker { display: none }
+							summary * { font: inherit; margin: 0; }
+							summary + div { left: 0; right: 0; padding: 1rem }
+							summary + div *:first-child { margin-top: 0 }
+							summary + div *:last-child { margin-bottom: 0 }
+						</style>
+						${this.innerHTML}
+					`
+				}
+
+				connectedCallback() {
+					this.sr.innerHTML = this.template
+
+					this.sr.querySelectorAll('summary').forEach((s) => {
+						s.setAttribute('part', 'tab')
+					})
+
+					this.sr.querySelectorAll('summary + *').forEach((s) => {
+						s.setAttribute('part', 'panel')
+					})
+
+					if (this.type === 'tabs') {
+						this.sr.innerHTML += `
+							<style>
+								:host { padding-top: var(--padding-top) }
+								details:not(:last-of-type) { border-bottom: 0 }
+								details:not(:last-of-type) summary {
+									border-right: 1px solid currentcolor;
+								}
+								summary { border-bottom: 1px solid currentcolor }
+								[part=indicator] {
+									background: white;
+									height: 2px;
+									position: absolute;
+									top: calc(var(--padding-top) - 2px);
+									transition: 0.33s;
+								}
+							</style>
+							<div part="indicator"></div>
+						`
+
+						const tabCount = this.children.length
+						const indicator = this.sr.querySelector('[part=indicator]')
+						const details = this.sr.querySelectorAll('details')
+
+						details.forEach((d, i) => {
+							if (d.open) this.open = i
+
+							const tab = d.querySelector('summary')
+							const panel = d.querySelector('summary + *')
+							const rm = matchMedia('prefers-reduced-motion').matches
+
+							this.setAttribute('style', '--padding-top: ' + tab.clientHeight + 'px')
+							tab.style.position = 'absolute'
+							tab.style.top = 0
+							tab.style.width = 100 / tabCount + '%'
+							tab.style.left = (100 / tabCount) * i + '%'
+
+							indicator.style.width = 100 / tabCount + '%'
+
+							tab.addEventListener('click', () => {
+								indicator.style.left = tab.style.left
+								if (!rm && i !== this.open) {
+									const direction = `translateX(${i < this.open ? '-' : ''}100%)`
+									panel.animate(
+										[
+											{ opacity: 0, transform: direction },
+											{ opacity: 0.33 },
+											{ opacity: '1', transform: 'translateX(0)' },
+										],
+										{
+											duration: 333,
+											iterations: 1,
+											easing: 'linear',
+										}
+									)
+								}
+								this.open = i
+								details.forEach((d) => (d.open = false))
+							})
+						})
+					}
+				}
+			}
+		)
+})()
