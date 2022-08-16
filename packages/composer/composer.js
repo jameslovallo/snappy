724c3d8a19beb3ac2859af3f08d7516c9f601d95
James Lovallo <lovallo.james@gmail.com>
2022-08-16 11:00:05 -0400

    make composer more flexible

--- packages/composer/composer.js
+++ packages/composer/composer.js
@@ -3,52 +3,37 @@ export default (c) => {
 		constructor() {
 			super()
 
-			// shadow dom or no?
-			this.DOM = c.shadow ? this.attachShadow({ mode: 'open' }) : this
-
 			// attach methods to component
-			if (c.methods) {
-				Object.keys(c.methods).forEach((key) => (this[key] = c.methods[key]))
-			}
+			Object.keys(c).forEach((key) => (this[key] = c[key]))
 
 			// get props and process values
-			if (c.props) {
-				this.props = {}
-				Object.keys(c.props).forEach((prop) => {
-					const func = c.props[prop]
-					this.props[prop] = {
-						value: this.getAttribute(prop),
-						handler: typeof func === 'string' ? this[func] : func,
-					}
-					this[prop] = this.props[prop].handler(this.getAttribute(prop))
-				})
-			}
+			const props = this.props()
+			Object.keys(props).forEach((prop) => {
+				const func = this.props()[prop]
+				this[prop] = func(this.getAttribute(prop))
+			})
+
+			// shadow dom or no?
+			this.DOM = this.shadow ? this.attachShadow({ mode: 'open' }) : this
 
 			// add template to dom and set up this.parts
-			if (c.template) {
-				this.template = c.template
-				if (c.styles) this.styles = c.styles
-				const css = this.styles ? `<style>${this.styles()}</style>` : ''
-				this.DOM.innerHTML = css + this.template()
-				this.parts = {}
-				this.DOM.querySelectorAll('[part]').forEach((part) => {
-					part.on = (type, callback) => {
-						callback = callback.bind(this);
-						part.addEventListener(type, (e) => callback(e));
-					};
-					this.parts[part.getAttribute('part')] = part
-				})
-			}
+			const css = this.styles ? `<style>${this.styles()}</style>` : ''
+			this.DOM.innerHTML = css + this.template()
+			this.parts = {}
+			this.DOM.querySelectorAll('[part]').forEach((part) => {
+				part.on = (type, callback) => {
+					callback = callback.bind(this)
+					part.addEventListener(type, (e) => callback(e))
+				}
+				this.parts[part.getAttribute('part')] = part
+			})
 		}
 
+		// run ready callback
 		connectedCallback() {
-			// run ready callback
-			if (c.ready) {
-				this.ready = c.ready
-				this.ready()
-			}
+			this.ready()
 		}
 	}
 
-	!customElements.get(c.name) && customElements.define(c.name, el)
+	!customElements.get(c.component) && customElements.define(c.component, el)
 }
